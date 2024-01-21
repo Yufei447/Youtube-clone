93ba4a4f547f9d7be5cc1dfedf9fefe799db3654
Yufei447 <ywang447@usc.edu>
2024-01-20 16:02:16 -0800

    add sign in with google button

---
+++ yt-web-client/app/firebase/firebase.ts
@@ -0,0 +1,41 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBH2xvUFJPiZNcNPD_ER1pDWe8NLEY2TiU",
+  authDomain: "clone-57e00.firebaseapp.com",
+  projectId: "clone-57e00",
+  appId: "1:213107190984:web:1c97f552f90f7da50b09fd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+  return signInWithPopup(auth, new GoogleAuthProvider());
+}
+
+/**
+ * Signs the user out.
+ * @returns A promise that resolves when the user is signed out.
+ */
+export function signOut() {
+  return auth.signOut();
+}
+
+/**
+ * Trigger a callback when user auth state changes.
+ * @returns A function to unsubscribe callback.
+ */
+export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+  return onAuthStateChanged(auth, callback);
+}
